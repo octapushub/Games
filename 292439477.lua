78f203abca80833a2f43992ab328ca0fd7ff27c6
Isa <nfleishman@student.ccaeagles.org>
2023-06-01 22:03:07 -0400

    Update 292439477.lua

--- 292439477.lua
+++ 292439477.lua
@@ -1,4 +1,5 @@
 CMDs[#CMDs + 1] = {NAME = 'phntm', DESC = 'Invite to the Infinite Yield support server.'}
+reloadCMDS()
 addcmd('phntm',{},function(args, speaker)
     notify('Welcome', 'Welcome to Phantom Forces')
 end
